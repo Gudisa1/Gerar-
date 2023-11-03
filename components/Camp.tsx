205999f473c930dbf0dd54e9fbcfaa1d385b0828
Gudisa Gebi <gudisagebi1@gmail.com>
2023-11-03 05:57:28 -0700

    GERAR COMPLETE

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -20,8 +20,8 @@ const CampSite = ({
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
-          <div className="rounded-full bg-green-50 p-4">
-            <Image src="/folded-map.svg" alt="map" width={28} height={28} />
+          <div className="rounded-full bg-black-50 p-4">
+            <Image src="/path5.svg" alt="map" width={28} height={28} />
           </div>
           <div className="flex flex-col gap-1">
             <h4 className="bold-18 text-white">{title}</h4>
--- components/Guide.tsx
+++ components/Guide.tsx
@@ -34,13 +34,13 @@ const Guide = () => {
         />
 
         <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
-          <Image
+          {/* <Image
             src="/meter.svg"
             alt="meter"
             width={16}
             height={158}
             className="h-full w-auto"
-          />
+          /> */}
           <div className="flexBetween flex-col">
             <div className="flex w-full flex-col">
               <div className="flexBetween w-full">
@@ -53,7 +53,7 @@ const Guide = () => {
             <div className="flex w-full flex-col">
               <p className="regular-16 text-gray-20">Begin Exploration</p>
               <h4 className="bold-20 mt-2 whitespace-nowrap">
-               Gerar Furniture
+                Gerar Furniture
               </h4>
             </div>
           </div>
